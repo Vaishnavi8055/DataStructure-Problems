4d9e39a2f21304ae7a3c7b47d2000d53cc7af8cd
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2021-05-21 05:50:17 +1400

    If size of array is even assign first pair of array elements in min and max variable respectively

---
+++ src/Array/Problem_5.java
@@ -0,0 +1,22 @@
+package Array;/*
+ * Created by Vaishnavi Chaurasia
+ * 01-Mar-21
+ * 8:06 AM
+ * Interview Problems
+ */
+
+// Find the maximum and minimum element in an array with minimum number of comparision
+// T(n) = O(n)
+// S(n) = O(1)
+public class Problem_5 {
+
+    public static void min_max_pairs(int arr[]){
+        int min = 0;
+        int max = 0;
+        // array size is even
+        if(arr.length % 2 == 0){
+            min = arr[0];
+            max = arr[1];
+        }
+    }
+}
