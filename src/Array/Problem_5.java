902c97e7f5ed5573a994ec7e775ca5055e7b7d3b
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2021-05-21 06:02:56 +1400

    Solution done

--- src/Array/Problem_5.java
+++ src/Array/Problem_5.java
@@ -10,18 +10,52 @@ package Array;/*
 // S(n) = O(1)
 public class Problem_5 {
 
-    public static void min_max_pairs(int arr[]){
+    public static void min_max_pairs(int arr[]) {
         int min = 0;
         int max = 0;
+        int i = 0;
         // array size is even
-        if(arr.length % 2 == 0){
+        if (arr.length % 2 == 0) {
             min = arr[0];
             max = arr[1];
+            i = 2;
         }
         // array size is odd
-        else{
-          min = arr[0];
-          max = arr[0];
+        else {
+            min = arr[0];
+            max = arr[0];
+            i = 1;
         }
+
+
+        //Compare in pairs
+
+        while (i < arr.length - 1) {
+
+            if (arr[i] > arr[i + 1]) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                if (arr[i + 1] < min) {
+                    min = arr[i + 1];
+                }
+            } else {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+                if (arr[i + 1] > max) {
+                    max = arr[i + 1];
+                }
+            }
+
+            i += 2; // As elements are compaired in pairs
+        }
+
+        System.out.println("Max - " + max + "," + "Min - " + min);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 3, 7, 80, 4};
+        min_max_pairs(arr);
     }
 }
