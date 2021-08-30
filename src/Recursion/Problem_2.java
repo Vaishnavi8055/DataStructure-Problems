b8f3719ae0600c1857a2e0cbec9220b424509e33
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2021-08-31 08:12:20 +1400

    Problem_2 done

---
+++ src/Recursion/Problem_2.java
@@ -0,0 +1,26 @@
+package Recursion;/*
+ * Created by Vaishnavi Chaurasia
+ * 31-Aug-21
+ * 7:11 AM
+ * DataStructure-Problems
+ */
+
+// WAP to print numbers from 1 to n in increasing order recursively.
+public class Problem_2 {
+
+    public static void printNumber(int n){
+
+        if(n == 0)
+          return;
+
+        printNumber(n-1);
+        System.out.print(n +" ");
+
+        return;
+    }
+
+
+    public static void main(String[] args) {
+        printNumber(5);
+    }
+}
