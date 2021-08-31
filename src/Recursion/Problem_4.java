fe779afcd67c2b86aee513b1556d86a26ca2238c
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2021-08-31 23:49:54 +1400

    Problem_4 done

---
+++ src/Recursion/Problem_4.java
@@ -0,0 +1,28 @@
+package Recursion;/*
+ * Created by Vaishnavi Chaurasia
+ * 31-Aug-21
+ * 11:35 PM
+ * DataStructure-Problems
+ */
+
+// Fibonacci Number
+public class Problem_4 {
+
+    public static int N_Fibo_Number(int n){
+
+        if(n == 0)
+            return 0;
+
+        if(n==1)
+            return 1;
+
+        return N_Fibo_Number(n-1) +  N_Fibo_Number(n-2);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(N_Fibo_Number(6));
+    }
+
+}
