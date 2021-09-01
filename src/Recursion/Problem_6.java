87e2ce60b6c96491831b81924456d6938ff13874
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2021-09-01 21:23:48 +1400

    Problem_6 done using method overriding

--- src/Recursion/Problem_6.java
+++ src/Recursion/Problem_6.java
@@ -10,19 +10,36 @@ public class Problem_6 {
 
     public static void main(String[] args) {
 
-        int arr[] = {1,2,3};
+        int arr[] = {1, 2, 3};
 
-        System.out.println(sumOfArray(arr,  0));
+       // System.out.println(sumOfArray(arr, 0));
+
+    // Just to hide startIndex logic from user, Now user need not to enter 0 as startIndex
+        System.out.println(sumOfArrayBetter(arr));
 
     }
 
-    public static int sumOfArray(int arr[] ,  int startIndex ) {
+//    public static int sumOfArray(int arr[], int startIndex) {
+//
+//        if (startIndex == arr.length)
+//            return 0;
+//
+//        return arr[startIndex] + sumOfArray(arr, startIndex + 1);
+//
+//    }
 
-        if(startIndex == arr.length)
+    // Method overriding
+    private static int sumOfArrayBetter (int arr[], int startIndex) {
+
+        if (startIndex == arr.length)
             return 0;
 
+        return arr[startIndex] + sumOfArrayBetter(arr, startIndex + 1);
 
-        return arr[startIndex] + sumOfArray(arr ,   startIndex+1 );
+    }
 
+    public static int sumOfArrayBetter(int arr[]) {
+
+       return sumOfArrayBetter(arr , 0);
     }
 }
